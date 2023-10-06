7eb22238616a720821190a29a58ccae7dc6c4275
Luke <lluminous.210@gmail.com>
2023-10-06 15:43:37 +0200

    fresh start

--- backend/test.mjs
+++
@@ -1,5 +0,0 @@
-import { createConnection } from 'typeorm';
-
-createConnection().then(async connection => {
-  console.log('Successfully connected to the database.');
-}).catch(error => console.log('Error: ', error));
