042f422ef96b4f82ff6ab37723fa97c373e7415a
Luke <lluminous.210@gmail.com>
2023-10-07 15:29:42 +0200

    adding auth and user logout

--- fullstack-vue-app/backend/src/middleware/authMiddleware.ts
+++ fullstack-vue-app/backend/src/middleware/authMiddleware.ts
@@ -2,6 +2,24 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export const protect = (req: Request, res: Response, next: NextFunction) => {
+  // List of routes that don't require authentication
+  const publicRoutes = [
+    { path: '/register', method: 'POST' },
+    { path: '/login', method: 'POST' },
+    // Add more routes here if needed
+  ];
+
+  // Check if the incoming request matches any of the public routes
+  const isPublicRoute = publicRoutes.some(
+    (route) => req.path === route.path && req.method === route.method
+  );
+
+  // If it's a public route, skip the token check
+  if (isPublicRoute) {
+    return next();
+  }
+
+  // Existing token check logic
   const token = req.header('Authorization');
 
   if (!token) {
@@ -16,4 +34,3 @@ export const protect = (req: Request, res: Response, next: NextFunction) => {
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
-
