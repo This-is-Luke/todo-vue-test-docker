7d31e3908e0bccdacd016e1388a4f9f711a70901
Luke <lluminous.210@gmail.com>
2023-10-10 14:58:50 +0200

    updated routes

--- fullstack-vue-app/frontend/src/App.vue
+++ fullstack-vue-app/frontend/src/App.vue
@@ -3,8 +3,11 @@
 <!-- eslint-disable vue/singleline-html-element-content-newline -->
 <template>
     <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/list">List</router-link> |
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/logout">Logout</router-link> |
+        <router-link to="/register">Register</router-link> |
+        <router-link to="/view-lists">View Lists</router-link>
     </nav>
     <router-view />
 </template>
