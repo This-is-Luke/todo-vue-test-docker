391223b7b5c421fe77cd0be365687c44ab1c234d
Luke <lluminous.210@gmail.com>
2023-10-16 13:51:36 +0200

    working frontend and backend, need to troubleshoot some things but its setup

--- fullstack-vue-app/frontend/src/main.ts
+++ fullstack-vue-app/frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
-import store from './store' // Make sure this path is correct
+import store from './store' 
 
 const app = createApp(App)
 
